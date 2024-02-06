7c03b95940b268d0426626b1aff19f613639dc37
riyeba <97381603+riyeba@users.noreply.github.com>
2024-02-06 20:53:29 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser
+./manage.py createsuperuser2 --username user --password 123 --noinput --email 'blank@email.com'
